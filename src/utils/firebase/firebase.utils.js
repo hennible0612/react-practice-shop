74fbbc26dcd6df244b998377aea8ea89678c3801
swh <sungwoo166@gmail.com>
2022-06-17 22:33:22 +0900

    added firestore
    .

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -38,4 +38,19 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     const userSnapshot = await getDoc(userDocRef);
     console.log(userSnapshot.exists()) //users가 있는지
 
+    if (!userSnapshot.exists()) { //User가 없을 경우
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef,{
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error){
+            console.log('error creating the user', error.message);
+        }
+    }
+
 };
