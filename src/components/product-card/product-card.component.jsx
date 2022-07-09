dcefe9dac0c5e1aee8f5d7725857b91f92c78876
swh <sungwoo166@gmail.com>
2022-07-09 13:30:45 +0900

    cart-item
    .

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,10 +1,16 @@
 import './product-card.styles.scss'
 import Button from "../button/button.component";
+import {useContext} from "react";
+import {CartContext} from "../../contexts/cart.context";
 
 
 const ProductCardComponent = ({product}) => {
-
     const {name, price, imageUrl} = product;
+    const {addItemToCart} = useContext(CartContext);
+    const addProductToCart = () => {
+        console.log("clicked")
+        addItemToCart(product)
+    }
 
 
     return (<div className="product-card-container">
@@ -13,7 +19,7 @@ const ProductCardComponent = ({product}) => {
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
-        <Button buttonType='inverted'>Add to card</Button>
+        <Button buttonType='inverted' onClick={addProductToCart}>Add to card</Button>
     </div>);
 };
 
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,16 +1,51 @@
-import {createContext, useState} from "react";
+import {createContext, useState, useEffect} from "react";
+import cartItemComponent from "../components/cart-item/cart-item.component";
+
+const addCartItem = (cartItems, productToAdd) => {
+
+    // find if carItems contains productToAdd
+    const existingCartItem = cartItems.find(
+        (cartItem) => // 함수의 조건에 있는 값을 return 한다
+        cartItem.id === productToAdd.id
+    );
+
+    // If found, increment quantity
+    if (existingCartItem) {
+        return cartItems.map((cartItem) => cartItem.id === productToAdd.id ?
+            {...cartItem, quantity: cartItem.quantity + 1} : cartItem);
+    }
+
+
+
+    // return new array with modified cartItems / new cart item
+    return [...cartItems, {...productToAdd, quantity: 1}];
+};
 
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {
-
+    setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {
     },
-
+    cartCount : 0,
 });
 
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = {isCartOpen, setIsCartOpen}
+    const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
+
+    useEffect(() => {
+        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity,0);
+        setCartCount(newCartCount)
+
+    }, [cartItems]); // cartItems가 바뀔때마다
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd))
+    };
+
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount}
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 };
